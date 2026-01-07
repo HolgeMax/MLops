bce37a17d7ea29ce62e3c6187315a1285ff04f9c
Holger Max Fløe Lyng <143646321+HolgeMax@users.noreply.github.com>
2026-01-07 09:55:29 +0100

    initialise repo MLOPS
    qa

---
+++ src/project_mlops/train.py
@@ -0,0 +1,10 @@
+from project_mlops.model import Model
+from project_mlops.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
